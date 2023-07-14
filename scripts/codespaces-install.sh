9a2415fffc7a60f56a5d426188878169ef9df270
Fabian Aguilar Gomez <tsusdere@github.com>
2023-07-14 17:45:35 -0500

    Update codespaces-install.sh

--- scripts/codespaces-install.sh
+++ scripts/codespaces-install.sh
@@ -20,9 +20,10 @@ if [[ -n "$HOMEASSISTANT_WEBHOOK_URL" ]]; then
     "$HOMEASSISTANT_WEBHOOK_URL"
 fi
 
+# TODO: add some configs here
 # remove existing init scripts
-rm -f "$HOME/.zshrc"
-rm -f "$HOME/.gitconfig"
+#rm -f "$HOME/.zshrc"
+#rm -f "$HOME/.gitconfig"
 
 packages_needed=(
   bat
