1764696fccbf3cc46f793e615dd97b89faeb387b
Fabian Aguilar Gomez <tsusdere@github.com>
2023-07-24 01:00:03 -0500

    Update nvim-tree.lua

--- config/nvim/after/plugin/nvim-tree.lua
+++ config/nvim/after/plugin/nvim-tree.lua
@@ -1,3 +1,3 @@
-vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>")
+vim.keymap.set("n", "<leader>nt", ":NvimTreeToggle<CR>")
 vim.keymap.set("n", "<leader>nr", ":NvimTreeRefresh<CR>")
 vim.keymap.set("n", "<leader>nf", ":NvimTreeFindFile<CR>")
