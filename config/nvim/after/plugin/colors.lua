8d7fa0b2063866b1543dd7f83884c987445f5b85
tsusdere <tsusdere@github.com>
2023-11-28 10:45:34 -0600

    test lazy

--- config/nvim/after/plugin/colors.lua
+++
@@ -1,9 +0,0 @@
-function Color(color)
-	color = color or 'catppuccin'
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { background = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { background = "none" })
-end
-
-Color()
--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,15 +1,2 @@
-require("tsusdere.packer")
-
-if not vim.env.HEADLESS_NEOVIM then
-  require("tsusdere.remap")
-  require("tsusdere.set")
-  local null_ls = require("null-ls")
-
-  null_ls.setup({
-      sources = {
-          null_ls.builtins.formatting.stylua,
-          null_ls.builtins.diagnostics.eslint,
-          null_ls.builtins.completion.spell,
-      },
-  })
-end
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
