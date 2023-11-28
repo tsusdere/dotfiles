97f77883caa5c92b660b804967bb2a9b999f5ef3
Fabian Aguilar Gomez <tsusdere@github.com>
2023-11-28 15:26:12 -0600

    Update linux-install.sh

--- scripts/linux-install.sh
+++ scripts/linux-install.sh
@@ -20,10 +20,6 @@ if [[ -n "$HOMEASSISTANT_WEBHOOK_URL" ]]; then
     "$HOMEASSISTANT_WEBHOOK_URL"
 fi
 
-# remove existing init scripts
-rm -f "$HOME/.zshrc"
-rm -f "$HOME/.gitconfig"
-
 packages_needed=(
   bat
   exuberant-ctags
