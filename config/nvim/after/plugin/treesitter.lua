b3cd308458902359595b2eeda58ea1c7d1d4bbbb
tsusdere <tsusdere@github.com>
2023-12-13 00:04:02 -0600

    swap to lazy

--- config/nvim/after/plugin/treesitter.lua
+++
@@ -1,8 +0,0 @@
-require("nvim-treesitter.configs").setup({
-    ensure_installed = {},
-    highlight = {
-        enable = true, 
-        additional_vim_regex_highlighting = false,
-        disable = {}, -- treesitter interferes with VimTex
-    },
-})
