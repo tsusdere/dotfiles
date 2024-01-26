dfdd4c436eaa6c39d21a344361cda248c06a3ec5
Fabian Aguilar Gomez <tsusdere@github.com>
2024-01-26 15:06:04 -0600

    Update colorscheme.lua

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -9,6 +9,14 @@ return {
       }
     end,
   },
-  { "arturgoms/moonbow.nvim" },
+  { "arturgoms/moonbow.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = function()
+      return {
+        transparent = true,
+      },
+    end,
+  },
   { "ellisonleao/gruvbox.nvim" },
 }
