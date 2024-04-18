144f3079ef9bb27b9afe598db8cfda61acbdee5b
Fabian Aguilar Gomez <tsusdere@github.com>
2024-04-17 19:18:40 -0500

    Update lazy.lua

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -13,7 +13,7 @@ require("lazy").setup({
       "LazyVim/LazyVim",
       import = "lazyvim.plugins",
       opts = {
-        colorscheme = "gruvbox",
+        colorscheme = "rose-pine",
         news = {
           lazyvim = true,
           neovim = true,
