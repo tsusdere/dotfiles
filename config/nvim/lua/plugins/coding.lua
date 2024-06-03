a568afbb1759c2c99ccc6dffb627bb7bdf492c02
Fabian Aguilar Gomez <tsusdere@github.com>
2024-06-02 23:54:17 -0500

    Update coding.lua

--- config/nvim/lua/plugins/coding.lua
+++ config/nvim/lua/plugins/coding.lua
@@ -92,6 +92,13 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
   },
   {
+  	"L3MON4D3/LuaSnip",
+  	-- follow latest release.
+  	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+  	-- install jsregexp (optional!).
+  	build = "make install_jsregexp"
+  },
+  {
     "nvim-cmp",
     dependencies = { "hrsh7th/cmp-emoji" },
     opts = function(_, opts)
