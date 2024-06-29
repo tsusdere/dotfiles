f7ec9cde8814c1a475b993e179ff5dbe8158657d
tsusdere <tsusdere@github.com>
2024-06-29 02:27:38 -0500

    fix theme

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -13,7 +13,7 @@ require("lazy").setup({
       "LazyVim/LazyVim",
       import = "lazyvim.plugins",
       opts = {
-        colorscheme = "kanagawa",
+        colorscheme = "kanagawa-dragon",
         news = {
           lazyvim = true,
           neovim = true,
--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -2,7 +2,6 @@ return {
   {
     "craftzdog/solarized-osaka.nvim",
     lazy = true,
-    priority = 1000,
     opts = function()
       return {
         transparent = true,
@@ -45,11 +44,67 @@ return {
   },
   {
     "rebelot/kanagawa.nvim",
+    lazy = false,
     config = function()
       require("kanagawa").setup({
-        theme = "night",
+        functionStyle = { bold = true },
+        dimInactive = true,
         transparent = true,
+        colors = {
+          palette = {
+            -- change all usages of these colors
+            sumiInk0 = "#000000",
+            fujiWhite = "#FFFFFF",
+          },
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = "none",
+              },
+            },
+            dragon = {
+              syn = {
+                parameter = "yellow",
+              },
+            },
+          },
+        },
+        overrides = function(colors)
+          local theme = colors.theme
+          return {
+            NormalFloat = { bg = "none" },
+            FloatBorder = { bg = "none" },
+            FloatTitle = { bg = "none" },
+
+            -- Save an hlgroup with dark background and dimmed foreground
+            -- so that you can use it where your still want darker windows.
+            -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+            NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+            Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+            PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+            PmenuSbar = { bg = theme.ui.bg_m1 },
+            PmenuThumb = { bg = theme.ui.bg_p2 },
+            -- Popular plugins that open floats will link to NormalFloat by default;
+            -- set their background accordingly if you wish to keep them dark and borderless
+            LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+            MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+            -- Assign a static color to strings
+            String = { fg = colors.palette.carpYellow, italic = true },
+            -- theme colors will update dynamically when you change theme!
+            SomePluginHl = { fg = colors.theme.syn.type, bold = true },
+            TelescopeTitle = { fg = theme.ui.special, bold = true },
+            TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+            TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+            TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+            TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+            TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+            TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+          }
+        end,
       })
+      vim.cmd("colorscheme kanagawa-dragon")
+      -- vim.cmd("colorscheme kanagawa-lotus")
     end,
   },
   { "ayu-theme/ayu-vim", transparent_mode = true },
