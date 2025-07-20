4ae643bf53ca77a0c47152403c7146214adb0ff0
Fabian Aguilar Gomez <tsusdere@github.com>
2025-07-20 01:56:29 -0500

    docker
    Enable Docker and clean up configuration comments.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -64,12 +60,6 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
@@ -109,6 +99,9 @@
      fish
   ];
 
+  # Docker
+  virtualisation.docker.enable = true;
+
   # Let's make fish the default shell
   programs.fish = {
    enable = true;
@@ -123,25 +116,8 @@
 
   users.defaultUserShell = pkgs.fish;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-
-  # Open ports in the firewall.
-# networking.firewall = {
-#   enable = true;
-#   allowedTCPPorts = [ 22 80 ];
-#   allowedUDPPorts = [ 3389 22 80 ];
-# };
 
   # GC
   nix.gc = {
