9c36be842936626a0e9fd0d2118de8e424bb661f
tsusdere <tsusdere@github.com>
2025-11-06 14:29:37 -0600

    update

---
+++ config/nvim/lua/custom/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		"craftzdog/solarized-osaka.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{
+		"vague-theme/vague.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other plugins
+		config = function()
+			-- NOTE: you do not need to call setup if you don't want to.
+			require("vague").setup({
+				transparent = true,
+			})
+			vim.cmd("colorscheme vague")
+		end,
+	},
+}
