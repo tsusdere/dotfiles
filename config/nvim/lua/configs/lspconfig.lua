6bc4beaa4d1b1da1cbf74bf97dd3c87b9aa4d68d
tsusdere <tsusdere@github.com>
2026-01-16 01:43:41 -0600

    lsp

--- config/nvim/lua/configs/lspconfig.lua
+++ config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,17 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local lspconfig = require('lspconfig')
+local nvlsp = require ("nvchad.configs.lspconfig")
+
+local servers = { "html", "cssls", "gopls", "rust_analyzer"}
 vim.lsp.enable(servers)
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
+end
 
 -- read :h vim.lsp.config for changing options of lsp servers 
