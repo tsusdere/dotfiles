3300b244ed27ab642a9b4176882a1c62257c0011
tsusdere <tsusdere@github.com>
2026-01-19 01:02:06 -0600

    fix neotest

---
+++ config/nvim/lua/configs/neotest.lua
@@ -0,0 +1,33 @@
+local neotest = require("neotest")
+
+neotest.setup({
+  adapters = {
+    require("neotest-go"),
+    require("neotest-python")({
+      dap = { justMyCode = false },
+    }),
+    require("neotest-jest")({
+      jestCommand = "npm test --",
+      jestConfigFile = "jest.config.js",
+      env = { CI = true },
+      cwd = function(_)
+        return vim.fn.getcwd()
+      end,
+    }),
+    require("neotest-plenary"),
+    require('rustaceanvim.neotest')
+  },
+
+  status = {
+    virtual_text = true,
+  },
+
+  output = {
+    open_on_run = false,
+  },
+
+  quickfix = {
+    open = false,
+  },
+})
+
