0de85aafe6f50f3cacd2bd1e52dfbe70ca0ae99b
tsusdere <tsusdere@github.com>
2026-01-22 20:00:03 -0600

    debugging feature

--- config/nvim/lua/configs/dap.lua
+++ config/nvim/lua/configs/dap.lua
@@ -1,25 +1,5 @@
-local dap, dapui = require "dap", require "dapui"
-
-require("dapui").setup()
-require("dap-go").setup()
-
-dap.listeners.before.attach.dapui_config = function()
-  dapui.open()
-end
-dap.listeners.before.launch.dapui_config = function()
-  dapui.open()
-end
-dap.listeners.before.event_terminated.dapui_config = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited.dapui_config = function()
-  dapui.close()
-end
-
-vim.keymap.set("n", "<Leader>dt", ":DapUiToggle<CR>", {})
-vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {})
-vim.keymap.set("n", "<Leader>dc", dap.continue, {})
-vim.keymap.set("n", "<Leader>dr", ":lua require('dapui').open({reset = true})<CR>", {})
+require("mason").setup()
+require("mason-nvim-dap").setup()
 
 vim.fn.sign_define(
   "DapBreakpoint",
